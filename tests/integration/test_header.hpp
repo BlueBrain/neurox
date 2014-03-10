211cd54859da826b8b0a03474bba200154b12d0a
Aleksandr Ovcharenko <aleksandr.ovcharenko@epfl.ch>
2014-03-10 14:58:41 +0100

    Restructuring folders to comply with Hello CMake framework
    Change-Id: I40f5b36dcf0d868eab98dd0e3c6b4f6d9a1c10b7

---
+++ tests/integration/test_header.hpp
@@ -0,0 +1,32 @@
+#ifndef COREBLURON_TEST_HEADER_HPP
+#define COREBLURON_TEST_HEADER_HPP
+
+#define BOOST_TEST_MODULE math
+#define BOOST_TEST_MAIN
+//#include <boost/test/included/unit_test.hpp>
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <string>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_case_template.hpp>
+
+#include "corebluron/nrnconf.h"
+#include "corebluron/nrnoc/multicore.h"
+#include "corebluron/nrnoc/nrnoc_decl.h"
+#include "corebluron/nrnmpi/nrnmpi.h"
+#include "corebluron/nrniv/nrniv_decl.h"
+#include "corebluron/nrniv/output_spikes.h"
+
+namespace corebluron {
+    namespace test {
+   
+    //something later
+
+    } // end namespace test
+} // end namespace COREBLURON
+
+#endif // COREBLURON_TEST_HEADER_HPP
