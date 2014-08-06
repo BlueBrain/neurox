729df15b0a841964591fbc820e3035660ac34438
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2014-08-06 14:13:18 +0200

    clang-format of Event VecPlayContinuous and NetCon class

--- neurox/VecPlayContinuous.h
+++ neurox/VecPlayContinuous.h
@@ -1,49 +1,48 @@
 #pragma once
 
-#include "neurox/neurox.h"
 #include <array>
+#include "neurox/neurox.h"
 
-namespace neurox
-{
+namespace neurox {
 
 /**
  * @brief The VecPlayContinuouX class
  * Equivalent to Coreneuron's VecPlayContinuous in nrniv/vrecitem.h
  * Includes/stores (?) information about continuous events
  */
-class VecPlayContinuousX : Event
-{
-  public:
-    VecPlayContinuousX() = delete;
-    VecPlayContinuousX(double*, size_t size_, floble_t * yvec, floble_t * tvec, floble_t * discon);
-    ~VecPlayContinuousX();
+class VecPlayContinuousX : Event {
+ public:
+  VecPlayContinuousX() = delete;
+  VecPlayContinuousX(double*, size_t size_, floble_t* yvec, floble_t* tvec,
+                     floble_t* discon);
+  ~VecPlayContinuousX();
 
-    void Deliver(floble_t t, Branch* branch); ///> at associated DiscreteEvent
-    void Continuous(floble_t tt); ///> play - every f(y, t) or res(y', y, t); record - advance_tn and initialize flag
-    void PlayInit(Branch * branch); ///> called near beginning of finitialize
+  /// delivers this event at a given branch and time
+  void Deliver(floble_t t, Branch* branch);
 
-    //methods from VectPlayContinuos (nrniv/vrecitem.h)
-    double Interpolate(double tt);
-    inline double Interp(double th, double x0, double x1) {
-        return x0 + (x1 - x0) * th;
-    }
-    void Search(double tt);
+  /// play - every f(y, t) or res(y', y, t); record - advance_tn and initialize flag
+  void Continuous(floble_t tt);
+  void PlayInit(Branch* branch);  ///> called near beginning of finitialize
 
-    virtual int Type() {
-        return VecPlayContinuousType;
-    }
+  // methods from VectPlayContinuos (nrniv/vrecitem.h)
+  double Interpolate(double tt);
+  inline double Interp(double th, double x0, double x1) {
+    return x0 + (x1 - x0) * th;
+  }
+  void Search(double tt);
 
-    //PlayRecord vars:
-    floble_t *pd_; ///>index to last value pointed by function continuous()
+  virtual int Type() { return VecPlayContinuousType; }
 
-    //VecPlayContinuoys vars
-    floble_t  * y_;
-    floble_t * t_;
-    floble_t* discon_indices_;
-    size_t size_; ///> size of arrays t_, y_
-    size_t last_index_;
-    size_t discon_index_;
-    size_t ubound_index_;
+  // PlayRecord vars:
+  floble_t* pd_;  ///>index to last value pointed by function continuous()
+
+  // VecPlayContinuoys vars
+  floble_t* y_;
+  floble_t* t_;
+  floble_t* discon_indices_;
+  size_t size_;  ///> size of arrays t_, y_
+  size_t last_index_;
+  size_t discon_index_;
+  size_t ubound_index_;
 };
-
 }
