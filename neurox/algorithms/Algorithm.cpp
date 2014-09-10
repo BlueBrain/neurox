685ff1e7eab4ccbe15e911d145e0b6872fc4d325
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2014-09-10 14:40:18 +0200

    clang-format of ./algorithms/*

--- neurox/algorithms/Algorithm.cpp
+++ neurox/algorithms/Algorithm.cpp
@@ -3,34 +3,31 @@
 using namespace neurox;
 using namespace neurox::algorithms;
 
-Algorithm* Algorithm::New(AlgorithmType type)
-{
-    switch (type)
-    {
+Algorithm* Algorithm::New(AlgorithmType type) {
+  switch (type) {
     case AlgorithmType::BackwardEulerDebug:
-        return new DebugAlgorithm();
+      return new DebugAlgorithm();
     case AlgorithmType::BackwardEulerCoreneuron:
-        return new CoreneuronAlgorithm();
+      return new CoreneuronAlgorithm();
     case AlgorithmType::BackwardEulerAllReduce:
-        return new AllReduceAlgorithm();
+      return new AllReduceAlgorithm();
     case AlgorithmType::BackwardEulerSlidingTimeWindow:
-        return new SlidingTimeWindowAlgorithm();
+      return new SlidingTimeWindowAlgorithm();
     case AlgorithmType::BackwardEulerTimeDependencyLCO:
-        return new TimeDependencyLCOAlgorithm();
+      return new TimeDependencyLCOAlgorithm();
     default:
-        return nullptr;
-    }
-    return nullptr;
+      return nullptr;
+  }
+  return nullptr;
 };
 
-void Algorithm::PrintStartInfo()
-{
-    printf("neurox::Algorithm::%s (%d neurons, t=%.03f secs, dt=%.03f milisecs\n",
-           getTypeString(), neurox::neurons_count, input_params->tstop/1000, input_params->dt);
-    fflush(stdout);
+void Algorithm::PrintStartInfo() {
+  printf("neurox::Algorithm::%s (%d neurons, t=%.03f secs, dt=%.03f milisecs\n",
+         getTypeString(), neurox::neurons_count, input_params->tstop / 1000,
+         input_params->dt);
+  fflush(stdout);
 }
 
-int Algorithm::getTotalStepsCount()
-{
-    return (input_params->tstop - input_params->tstart) / input_params->dt;
+int Algorithm::getTotalStepsCount() {
+  return (input_params->tstop - input_params->tstart) / input_params->dt;
 }
