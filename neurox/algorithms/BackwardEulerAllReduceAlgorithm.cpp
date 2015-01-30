f818c1097f0f4fff3a39b7f090a0c3ab6b830a11
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2015-01-30 16:16:53 +0200

    improved Algorith.h header

--- neurox/algorithms/BackwardEulerAllReduceAlgorithm.cpp
+++ neurox/algorithms/BackwardEulerAllReduceAlgorithm.cpp
@@ -53,9 +53,7 @@ void DERIVED_CLASS_NAME::StepEnd(Branch* b, hpx_t spikesLco)
     input::Debugger::SingleNeuronStepAndCompare(&nrn_threads[b->nt->id], b, inputParams->secondorder);
 }
 
-void DERIVED_CLASS_NAME::CommStepBegin(Branch*) {}
-
-void DERIVED_CLASS_NAME::CommStepEnd(Branch*) {}
+void DERIVED_CLASS_NAME::Run(Branch*) {}
 
 hpx_t DERIVED_CLASS_NAME::SendSpikes(Neuron* b, double tt, double)
 {
