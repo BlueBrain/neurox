928d16c1aa3c7ab9199a5659496f035da5a47256
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2015-02-04 19:14:16 +0200

    renamed CoreneuronDebugAlgorithm to DebugAlgorithm

--- neurox/tools/CmdLineParser.cpp
+++ neurox/tools/CmdLineParser.cpp
@@ -44,12 +44,12 @@ void CmdLineParser::Parse(int argc, char ** argv)
         TCLAP::ValueArg<int> branchingDepth("B","multisplix","depth tree-based parallelism of morphologies (0 = no branch parallelism, default)", false, 0, "int");
         cmd.add(branchingDepth);
         TCLAP::ValueArg<int> algorithm("A","algorithm",
-                                       "BackwardEulerCoreneuronDebug [-1],\
-                                        BackwardEulerCoreneuron [0],\
+                                       "BackwardEulerCoreneuron [0],\
                                         BackwardEulerWithAllReduceBarrier [1] (default),\
                                         BackwardEulerWithSlidingTimeWindow [2],\
                                         BackwardEulerWithTimeDependencyLCO [3],\
-                                        All methods sequentially (NOTE: neurons data not reset) [9]",
+                                        BackwardEulerCoreneuronDebug [4],\
+                                        All methods sequentially (NOTE: neurons data does not reset) [9]",
                                        false, 1, "int");
         cmd.add(algorithm);
 
