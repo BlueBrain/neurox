accddd482fb3006df18fa6bfd08603355148a008
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2015-04-24 11:28:46 +0200

    created Init and Clear functions for AllReduce-based algorithms

--- neurox/algorithms/BackwardEulerAllReduceAlgorithm.cpp
+++ neurox/algorithms/BackwardEulerAllReduceAlgorithm.cpp
@@ -3,6 +3,8 @@
 using namespace neurox;
 using namespace neurox::algorithms;
 
+hpx_t* DERIVED_CLASS_NAME::allReduces = nullptr;
+
 DERIVED_CLASS_NAME::DERIVED_CLASS_NAME() {}
 
 DERIVED_CLASS_NAME::~DERIVED_CLASS_NAME() {}
@@ -12,16 +14,59 @@ AlgorithmType DERIVED_CLASS_NAME::getType()
     return AlgorithmType::BackwardEulerAllReduce;
 }
 
-void DERIVED_CLASS_NAME::init(Branch*) {}
+void DERIVED_CLASS_NAME::Init() {
+    SubscribeAllReduces(DERIVED_CLASS_NAME::allReduces,
+                        DERIVED_CLASS_NAME::allReducesCount);
+}
 
-void DERIVED_CLASS_NAME::clear(Branch*) {}
+void DERIVED_CLASS_NAME::Clear() {
+    UnsubscribeAllReduces(DERIVED_CLASS_NAME::allReduces,
+                          DERIVED_CLASS_NAME::allReducesCount);
+}
 
-void DERIVED_CLASS_NAME::stepBegin(Branch*) {}
+void DERIVED_CLASS_NAME::StepBegin(Branch*) {}
 
-void DERIVED_CLASS_NAME::stepEnd(Branch*) {}
+void DERIVED_CLASS_NAME::StepEnd(Branch*) {}
 
-void DERIVED_CLASS_NAME::commStepBegin(Branch*) {}
+void DERIVED_CLASS_NAME::CommStepBegin(Branch*) {}
 
-void DERIVED_CLASS_NAME::commStepEnd(Branch*) {}
+void DERIVED_CLASS_NAME::CommStepEnd(Branch*) {}
 
-void DERIVED_CLASS_NAME::afterSpike(Branch*) {}
+void DERIVED_CLASS_NAME::AfterSpike(Branch*) {}
+
+void DERIVED_CLASS_NAME::SubscribeAllReduces(hpx_t *& allReduces, size_t allReducesCount)
+{
+    assert(allReduces==nullptr);
+    allReduces = new hpx_t[allReducesCount];
+
+    hpx_bcast_rsync(neurox::Neuron::SlidingTimeWindow::SetReductionsPerCommStep, &allReducesCount, sizeof(int));
+
+    for (int i=0; i<allReducesCount; i++)
+        allReduces[i] = hpx_process_collective_allreduce_new(0, Neuron::SlidingTimeWindow::Init, Neuron::SlidingTimeWindow::Reduce);
+
+    if (inputParams->allReduceAtLocality)
+        hpx_bcast_rsync(neurox::Neuron::SlidingTimeWindow::AllReduceLocality::SubscribeAllReduce,
+                        allReduces, sizeof(hpx_t)*allReducesCount);
+    else
+        neurox_hpx_call_neurons_lco(Neuron::SlidingTimeWindow::SubscribeAllReduce,
+                        allReduces, sizeof(hpx_t)*allReducesCount);
+
+    for (int i=0; i<allReducesCount; i++)
+        hpx_process_collective_allreduce_subscribe_finalize(allReduces[i]);
+}
+
+void DERIVED_CLASS_NAME::UnsubscribeAllReduces(hpx_t *& allReduces, size_t allReducesCount)
+{
+    assert(allReduces!=nullptr);
+    if (inputParams->allReduceAtLocality)
+        hpx_bcast_rsync(neurox::Neuron::SlidingTimeWindow::AllReduceLocality::UnsubscribeAllReduce,
+                        allReduces, sizeof(hpx_t)*allReducesCount);
+    else
+        neurox_hpx_call_neurons_lco(Neuron::SlidingTimeWindow::UnsubscribeAllReduce,
+                        allReduces, sizeof(hpx_t)*allReducesCount);
+
+    for (int i=0; i<allReducesCount; i++)
+        hpx_process_collective_allreduce_delete(allReduces[i]);
+
+    delete [] allReduces; allReduces=nullptr;
+}
