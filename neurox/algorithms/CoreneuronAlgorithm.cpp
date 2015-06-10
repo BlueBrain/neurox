708a9a5e7e6c72e84c583da017f772837a9d3ceb
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2015-06-10 14:40:18 +0200

    clang-format of ./algorithms/*

--- neurox/algorithms/CoreneuronAlgorithm.cpp
+++ neurox/algorithms/CoreneuronAlgorithm.cpp
@@ -7,61 +7,41 @@ CoreneuronAlgorithm::CoreneuronAlgorithm() {}
 
 CoreneuronAlgorithm::~CoreneuronAlgorithm() {}
 
-CoreneuronAlgorithm::CommunicationBarrier::CommunicationBarrier()
-{
-    assert(0);
+CoreneuronAlgorithm::CommunicationBarrier::CommunicationBarrier() { assert(0); }
+
+CoreneuronAlgorithm::CommunicationBarrier::~CommunicationBarrier() {
+  assert(0);
 }
 
-CoreneuronAlgorithm::CommunicationBarrier::~CommunicationBarrier()
-{
-    assert(0);
+const AlgorithmType CoreneuronAlgorithm::getType() {
+  return AlgorithmType::BackwardEulerCoreneuron;
 }
 
-const AlgorithmType CoreneuronAlgorithm::getType()
-{
-    return AlgorithmType::BackwardEulerCoreneuron;
+const char* CoreneuronAlgorithm::getTypeString() {
+  return "BackwardEulerCoreneuron";
 }
 
-const char* CoreneuronAlgorithm::getTypeString()
-{
-    return "BackwardEulerCoreneuron";
-}
-
-void CoreneuronAlgorithm::Init()
-{
-    assert(0);
-}
+void CoreneuronAlgorithm::Init() { assert(0); }
 
 void CoreneuronAlgorithm::Clear() {}
 
-double CoreneuronAlgorithm::Launch()
-{
-    int commStepSize = CoreneuronAlgorithm::CommunicationBarrier::commStepSize;
-    int totalSteps = Algorithm::getTotalStepsCount();
-    hpx_time_t now = hpx_time_now();
-    assert(0);
-    double elapsedTime = hpx_time_elapsed_ms(now)/1e3;
-    input::Debugger::CompareAllBranches();
-    return elapsedTime;
+double CoreneuronAlgorithm::Launch() {
+  int commStepSize = CoreneuronAlgorithm::CommunicationBarrier::commStepSize;
+  int totalSteps = Algorithm::getTotalStepsCount();
+  hpx_time_t now = hpx_time_now();
+  assert(0);
+  double elapsedTime = hpx_time_elapsed_ms(now) / 1e3;
+  input::Debugger::CompareAllBranches();
+  return elapsedTime;
 }
 
-void CoreneuronAlgorithm::StepBegin(Branch*)
-{
-    assert(0);
-}
+void CoreneuronAlgorithm::StepBegin(Branch*) { assert(0); }
 
-void CoreneuronAlgorithm::StepEnd(Branch* b, hpx_t)
-{
-    assert(0);
-}
+void CoreneuronAlgorithm::StepEnd(Branch* b, hpx_t) { assert(0); }
 
-void CoreneuronAlgorithm::Run(Branch* b, const void* args)
-{
-    assert(0);
-}
+void CoreneuronAlgorithm::Run(Branch* b, const void* args) { assert(0); }
 
-hpx_t CoreneuronAlgorithm::SendSpikes(Neuron* neuron, double tt, double)
-{
-    assert(0);
-    return HPX_NULL;
+hpx_t CoreneuronAlgorithm::SendSpikes(Neuron* neuron, double tt, double) {
+  assert(0);
+  return HPX_NULL;
 }
--- neurox/algorithms/CoreneuronAlgorithm.h
+++ neurox/algorithms/CoreneuronAlgorithm.h
@@ -3,40 +3,36 @@
 
 using namespace neurox;
 
-namespace neurox
-{
+namespace neurox {
 
-namespace algorithms
-{
+namespace algorithms {
 
-class CoreneuronAlgorithm : public Algorithm
-{
-  public:
-    CoreneuronAlgorithm();
-    ~CoreneuronAlgorithm();
+class CoreneuronAlgorithm : public Algorithm {
+ public:
+  CoreneuronAlgorithm();
+  ~CoreneuronAlgorithm();
 
-    const AlgorithmType getType() override;
-    const char* getTypeString() override;
+  const AlgorithmType getType() override;
+  const char* getTypeString() override;
 
-    void Init() override;
-    void Clear() override;
-    double Launch() override;
+  void Init() override;
+  void Clear() override;
+  double Launch() override;
 
-    void StepBegin(Branch*) override;
-    void StepEnd(Branch*, hpx_t) override;
-    void Run(Branch*, const void*) override;
-    hpx_t SendSpikes(Neuron*, double, double) override;
+  void StepBegin(Branch*) override;
+  void StepEnd(Branch*, hpx_t) override;
+  void Run(Branch*, const void*) override;
+  hpx_t SendSpikes(Neuron*, double, double) override;
 
-    class CommunicationBarrier : public AlgorithmMetaData
-    {
-      public:
-        CommunicationBarrier();
-        ~CommunicationBarrier();
+  class CommunicationBarrier : public AlgorithmMetaData {
+   public:
+    CommunicationBarrier();
+    ~CommunicationBarrier();
 
-        static constexpr int commStepSize = 4; ///> Fixed communication step size
-    };
+    static constexpr int commStepSize = 4;  ///> Fixed communication step size
+  };
 };
 
-}; //algorithm
+};  // algorithm
 
-}; //neurox
+};  // neurox
