e2fe4c9a808b907831e806fe316b476d26b2f4a4
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2015-06-19 14:43:21 +0200

    clang-format of ./tools/*

--- neurox/tools/Vectorizer.h
+++ neurox/tools/Vectorizer.h
@@ -2,10 +2,10 @@
 
 #include "neurox/neurox.h"
 
-#include <map>
-#include <vector>
-#include <memory>
 #include <deque>
+#include <map>
+#include <memory>
+#include <vector>
 
 using namespace std;
 
@@ -17,37 +17,34 @@ namespace tools {
  * @brief The Vectorizer class
  * converts code from AoS to SoA structure
  */
-class Vectorizer
-{
-  public:
-    Vectorizer()=delete;
-    ~Vectorizer()=delete;
+class Vectorizer {
+ public:
+  Vectorizer() = delete;
+  ~Vectorizer() = delete;
 
-    ///converts a branch from AoS to SoA
-    static void ConvertToSOA(Branch * b);
+  /// converts a branch from AoS to SoA
+  static void ConvertToSOA(Branch* b);
 
-    ///get size of datastructure with alignment-based padding
-    static size_t SizeOf(size_t size)
-    {
-        return coreneuron::soa_padded_size<NEUROX_SOA_PADDING>(size, LAYOUT);
-    }
+  /// get size of datastructure with alignment-based padding
+  static size_t SizeOf(size_t size) {
+    return coreneuron::soa_padded_size<NEUROX_SOA_PADDING>(size, LAYOUT);
+  }
 
-    //C++11 does not support memory-aligned new[]/delete, this is a work around
-    template<typename T>
-    static T* New(size_t count)
-    {
-        return (T*)coreneuron::ecalloc_align(SizeOf(count), NEUROX_MEM_ALIGNMENT, sizeof(T));
-    }
+  // C++11 does not support memory-aligned new[]/delete, this is a work around
+  template <typename T>
+  static T* New(size_t count) {
+    return (T*)coreneuron::ecalloc_align(SizeOf(count), NEUROX_MEM_ALIGNMENT,
+                                         sizeof(T));
+  }
 
-    template<typename T>
-    static void Delete(T * ptr)
-    {
-        free(ptr); ptr=nullptr; //delete[] (ptr);
-    }
+  template <typename T>
+  static void Delete(T* ptr) {
+    free(ptr);
+    ptr = nullptr;  // delete[] (ptr);
+  }
 
-  private:
+ private:
 };
 
-}; //Tools
-}; //NeuroX
-
+};  // Tools
+};  // NeuroX
