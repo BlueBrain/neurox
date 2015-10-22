4f2949ead07b36357711892881152a16f869082d
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2015-10-22 14:36:59 +0200

    added *Algorithm.cpp

---
+++ neurox/algorithms/BackwardEulerTimeDependencyLCOAlgorithm.cpp
@@ -0,0 +1,27 @@
+#include "neurox/algorithms/BackwardEulerTimeDependencyLCOAlgorithm.h"
+
+using namespace neurox;
+using namespace neurox::algorithms;
+
+DERIVED_CLASS_NAME::DERIVED_CLASS_NAME() {}
+
+DERIVED_CLASS_NAME::~DERIVED_CLASS_NAME() {}
+
+AlgorithmType DERIVED_CLASS_NAME::getType()
+{
+    return AlgorithmType::BackwardEulerAllReduce;
+}
+
+void DERIVED_CLASS_NAME::init(Branch*) {}
+
+void DERIVED_CLASS_NAME::clear(Branch*) {}
+
+void DERIVED_CLASS_NAME::stepBegin(Branch*) {}
+
+void DERIVED_CLASS_NAME::stepEnd(Branch*) {}
+
+void DERIVED_CLASS_NAME::commStepBegin(Branch*) {}
+
+void DERIVED_CLASS_NAME::commStepEnd(Branch*) {}
+
+void DERIVED_CLASS_NAME::afterSpike(Branch*) {}
