176ff44f3326b8211af1466d55d431a2979d1a10
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2015-10-25 14:17:56 +0200

    added Algorithm::Run() instantiations

--- neurox/algorithms/BackwardEulerDebugModeAlgorithm.cpp
+++ neurox/algorithms/BackwardEulerDebugModeAlgorithm.cpp
@@ -17,9 +17,40 @@ const char* DERIVED_CLASS_NAME::getTypeString()
     return "BackwardEulerDebugMode";
 }
 
-void DERIVED_CLASS_NAME::Init() {}
+void DERIVED_CLASS_NAME::Init()
+{
+    const int allReducesCount = 0;
+    hpx_bcast_rsync(neurox::Neuron::SlidingTimeWindow::SetReductionsPerCommStep,
+                    &allReducesCount, sizeof(int));
+}
 
-void DERIVED_CLASS_NAME::Clear() {}
+void DERIVED_CLASS_NAME::Finalize() {}
+
+double DERIVED_CLASS_NAME::Run()
+{
+    int commStepSize = Neuron::CommunicationBarrier::commStepSize;
+    int totalSteps = Algorithm::getTotalStepsCount();
+
+    hpx_time_t now = hpx_time_now();
+    for (int s=0; s<totalSteps; s+=Neuron::CommunicationBarrier::commStepSize)
+    {
+        #ifdef NEUROX_TIME_STEPPING_VERBOSE
+          if (hpx_get_my_rank()==0)
+            DebugMessage(std::string("-- t="+std::to_string(inputParams->dt*s)+" ms\n").c_str());
+        #endif
+
+        //Reduction at locality is not implemented (this mode is for debugging only)
+        neurox_hpx_call_neurons_lco(Branch::BackwardEuler, &commStepSize, sizeof(int));
+
+        #ifndef NDEBUG
+          if (inputParams->parallelDataLoading) //if parallel execution... spike exchange
+            hpx_bcast_rsync(neurox::input::Debugger::NrnSpikeExchange);
+        #endif
+    }
+    double elapsedTime = hpx_time_elapsed_ms(now)/1e3;
+    input::Debugger::CompareAllBranches();
+    return elapsedTime;
+}
 
 void DERIVED_CLASS_NAME::StepBegin(Branch*) {}
 
