e5b003deb9afd2e5b72aa34289b2e0eee212a1fc
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2015-12-10 14:40:18 +0200

    clang-format of ./algorithms/*

--- neurox/algorithms/AlgorithmMetaData.cpp
+++ neurox/algorithms/AlgorithmMetaData.cpp
@@ -3,22 +3,20 @@
 using namespace neurox;
 using namespace neurox::algorithms;
 
-AlgorithmMetaData* AlgorithmMetaData::New(AlgorithmType type)
-{
-    switch (type)
-    {
+AlgorithmMetaData* AlgorithmMetaData::New(AlgorithmType type) {
+  switch (type) {
     case AlgorithmType::BackwardEulerDebug:
-        return new DebugAlgorithm::CommunicationBarrier();
+      return new DebugAlgorithm::CommunicationBarrier();
     case AlgorithmType::BackwardEulerCoreneuron:
-        return new CoreneuronAlgorithm::CommunicationBarrier();
+      return new CoreneuronAlgorithm::CommunicationBarrier();
     case AlgorithmType::BackwardEulerAllReduce:
-        return new AllReduceAlgorithm::AllReducesInfo();
+      return new AllReduceAlgorithm::AllReducesInfo();
     case AlgorithmType::BackwardEulerSlidingTimeWindow:
-        return new AllReduceAlgorithm::AllReducesInfo();
+      return new AllReduceAlgorithm::AllReducesInfo();
     case AlgorithmType::BackwardEulerTimeDependencyLCO:
-        return new TimeDependencyLCOAlgorithm::TimeDependencies();
+      return new TimeDependencyLCOAlgorithm::TimeDependencies();
     default:
-        return nullptr;
-    }
-    return nullptr;
+      return nullptr;
+  }
+  return nullptr;
 }
--- neurox/algorithms/SlidingTimeWindowAlgorithm.cpp
+++ neurox/algorithms/SlidingTimeWindowAlgorithm.cpp
@@ -5,62 +5,59 @@ using namespace neurox::algorithms;
 
 hpx_t* SlidingTimeWindowAlgorithm::allReduces = nullptr;
 
-SlidingTimeWindowAlgorithm::SlidingTimeWindowAlgorithm()
-{
-    AllReduceAlgorithm::AllReducesInfo::reductionsPerCommStep = SlidingTimeWindowAlgorithm::allReducesCount;
+SlidingTimeWindowAlgorithm::SlidingTimeWindowAlgorithm() {
+  AllReduceAlgorithm::AllReducesInfo::reductionsPerCommStep =
+      SlidingTimeWindowAlgorithm::allReducesCount;
 }
 
 SlidingTimeWindowAlgorithm::~SlidingTimeWindowAlgorithm() {}
 
-const AlgorithmType SlidingTimeWindowAlgorithm::getType()
-{
-    return AlgorithmType::BackwardEulerSlidingTimeWindow;
+const AlgorithmType SlidingTimeWindowAlgorithm::getType() {
+  return AlgorithmType::BackwardEulerSlidingTimeWindow;
 }
 
-const char* SlidingTimeWindowAlgorithm::getTypeString()
-{
-    return "BackwardEulerSlidingTimeWindow";
+const char* SlidingTimeWindowAlgorithm::getTypeString() {
+  return "BackwardEulerSlidingTimeWindow";
 }
 
 void SlidingTimeWindowAlgorithm::Init() {
-    AllReduceAlgorithm::SubscribeAllReduces(
-                SlidingTimeWindowAlgorithm::allReduces,
-                SlidingTimeWindowAlgorithm::allReducesCount);
+  AllReduceAlgorithm::SubscribeAllReduces(
+      SlidingTimeWindowAlgorithm::allReduces,
+      SlidingTimeWindowAlgorithm::allReducesCount);
 }
 
 void SlidingTimeWindowAlgorithm::Clear() {
-    AllReduceAlgorithm::UnsubscribeAllReduces(
-                SlidingTimeWindowAlgorithm::allReduces,
-                SlidingTimeWindowAlgorithm::allReducesCount);
+  AllReduceAlgorithm::UnsubscribeAllReduces(
+      SlidingTimeWindowAlgorithm::allReduces,
+      SlidingTimeWindowAlgorithm::allReducesCount);
 }
 
-double SlidingTimeWindowAlgorithm::Launch()
-{
-    int totalSteps = Algorithm::getTotalStepsCount();
-    hpx_time_t now = hpx_time_now();
-    if (input_params->allReduceAtLocality)
-        hpx_bcast_rsync(Branch::BackwardEulerOnLocality, &totalSteps, sizeof(int));
-    else
-        neurox_hpx_call_neurons_lco(Branch::BackwardEuler, &totalSteps, sizeof(int));
-    double elapsedTime = hpx_time_elapsed_ms(now)/1e3;
-    input::Debugger::RunCoreneuronAndCompareAllBranches();
-    return elapsedTime;
+double SlidingTimeWindowAlgorithm::Launch() {
+  int totalSteps = Algorithm::getTotalStepsCount();
+  hpx_time_t now = hpx_time_now();
+  if (input_params->allReduceAtLocality)
+    hpx_bcast_rsync(Branch::BackwardEulerOnLocality, &totalSteps, sizeof(int));
+  else
+    neurox_hpx_call_neurons_lco(Branch::BackwardEuler, &totalSteps,
+                                sizeof(int));
+  double elapsedTime = hpx_time_elapsed_ms(now) / 1e3;
+  input::Debugger::RunCoreneuronAndCompareAllBranches();
+  return elapsedTime;
 }
 
 void SlidingTimeWindowAlgorithm::StepBegin(Branch*) {}
 
-void SlidingTimeWindowAlgorithm::StepEnd(Branch* b, hpx_t spikesLco)
-{
-    AllReduceAlgorithm::WaitForSpikesDelivery(b, spikesLco);
-    input::Debugger::SingleNeuronStepAndCompare(&nrn_threads[b->nt->id], b, input_params->secondorder);
+void SlidingTimeWindowAlgorithm::StepEnd(Branch* b, hpx_t spikesLco) {
+  AllReduceAlgorithm::WaitForSpikesDelivery(b, spikesLco);
+  input::Debugger::SingleNeuronStepAndCompare(&nrn_threads[b->nt->id], b,
+                                              input_params->secondorder);
 }
 
-void SlidingTimeWindowAlgorithm::Run(Branch*b, const void* args)
-{
-    AllReduceAlgorithm::Run2(b, args);
+void SlidingTimeWindowAlgorithm::Run(Branch* b, const void* args) {
+  AllReduceAlgorithm::Run2(b, args);
 }
 
-hpx_t SlidingTimeWindowAlgorithm::SendSpikes(Neuron* neuron, double tt, double)
-{
-    return AllReduceAlgorithm::SendSpikes2(neuron,tt);
+hpx_t SlidingTimeWindowAlgorithm::SendSpikes(Neuron* neuron, double tt,
+                                             double) {
+  return AllReduceAlgorithm::SendSpikes2(neuron, tt);
 }
