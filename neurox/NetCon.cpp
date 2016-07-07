4263db18e248502ce2e28aaaec1dfb3c21e759ba
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2016-07-07 14:13:18 +0200

    clang-format of Event VecPlayContinuous and NetCon class

--- neurox/NetCon.cpp
+++ neurox/NetCon.cpp
@@ -1,22 +1,23 @@
-#include "neurox/neurox.h"
 #include <cstring>
+#include "neurox/neurox.h"
 
 using namespace neurox;
 
-NetConX::NetConX(){}
+NetConX::NetConX() {}
 
 NetConX::NetConX(int mechType, offset_t mechInstance, floble_t delay,
                  offset_t weightIndex, unsigned short weightsCount, bool active)
-    :mechType(mechType), weightsCount(weightsCount), mechInstance(mechInstance),
-      delay(delay), weightIndex(weightIndex), active(active)
-{}
+    : mechType(mechType),
+      weightsCount(weightsCount),
+      mechInstance(mechInstance),
+      delay(delay),
+      weightIndex(weightIndex),
+      active(active) {}
 
-NetConX::~NetConX()
-{}
+NetConX::~NetConX() {}
 
-void NetConX::Deliver(floble_t tt, Branch* branch)
-{
-    if (this->active)
-        GetMechanismFromType(mechType)->CallModFunction(branch, Mechanism::ModFunction::netReceive, this, tt);
+void NetConX::Deliver(floble_t tt, Branch* branch) {
+  if (this->active)
+    GetMechanismFromType(mechType)->CallModFunction(
+        branch, Mechanism::ModFunction::netReceive, this, tt);
 }
-
--- neurox/NetCon.h
+++ neurox/NetCon.h
@@ -2,42 +2,45 @@
 
 #include "neurox/neurox.h"
 
-namespace neurox
-{
+namespace neurox {
 
 /**
  * @brief The NetCon class
  * Equivalent to Coreneuron's NetCon in netcon.h
  * Includes the synaptic information at the post-synaptic neuron
  */
-class NetConX : Event
-{
-  public:
-    NetConX();
-    NetConX(int mechType, offset_t mechInstance, floble_t delay,
-            offset_t weightIndex, unsigned short weightsCount, bool active);
-    ~NetConX();
+class NetConX : Event {
+ public:
+  NetConX();
+  NetConX(int mechType, offset_t mechInstance, floble_t delay,
+          offset_t weightIndex, unsigned short weightsCount, bool active);
+  ~NetConX();
 
-    void Deliver(floble_t t, Branch* branch) override; //event method (inherited)
+  void Deliver(floble_t t, Branch* branch) override;  // event method
+                                                      // (inherited)
 
-    int mechType;   ///> mechanism type associated with this synapse
-    unsigned short weightsCount;  ///> size of variable args
-    bool active;          ///> decides whether NetCon is active (or not)
-    floble_t delay;       ///> synaptic (soma-bouton distance + transmitters release delay)
-    offset_t weightIndex;      ///> net_receive arguments (equivalent to weights in Coreneuron's NetCon)
-    offset_t mechInstance;     ///> mechanism instance, from the mechanism type
+  int mechType;                 ///> mechanism type associated with this synapse
+  unsigned short weightsCount;  ///> size of variable args
+  bool active;                  ///> decides whether NetCon is active (or not)
 
-  private:
+  /// synaptic delay (soma-bouton distance + transmitters release delay)
+  floble_t delay;
+
+  ///  net_receive arguments (equivalent to weights in Coreneuron's NetCon)
+  offset_t weightIndex;
+
+  /// mechanism instance, from the mechanism type
+  offset_t mechInstance;
+
+ private:
 };
 
-///temp wrapper for point process
-struct PointProcInfo
-{
-    offset_t nodeId; //compartment id
-    int mechType;
-    offset_t mechInstance;
-    offset_t instanceDataOffset;
-    size_t size;
+/// temp wrapper for point process
+struct PointProcInfo {
+  offset_t nodeId;  // compartment id
+  int mechType;
+  offset_t mechInstance;
+  offset_t instanceDataOffset;
+  size_t size;
 };
-
 }
