972cfe1d7df59cae3baf9403ca79047c2d87d2d9
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2016-07-10 14:40:18 +0200

    clang-format of ./algorithms/*

--- neurox/algorithms/TimeDependencyLCOAlgorithm.h
+++ neurox/algorithms/TimeDependencyLCOAlgorithm.h
@@ -3,58 +3,73 @@
 
 using namespace neurox;
 
-namespace neurox
-{
+namespace neurox {
 
-namespace algorithms
-{
+namespace algorithms {
 
-class TimeDependencyLCOAlgorithm : public Algorithm
-{
-  public:
-    TimeDependencyLCOAlgorithm();
-    ~TimeDependencyLCOAlgorithm();
+class TimeDependencyLCOAlgorithm : public Algorithm {
+ public:
+  TimeDependencyLCOAlgorithm();
+  ~TimeDependencyLCOAlgorithm();
 
-    const AlgorithmType getType() override;
-    const char* getTypeString() override;
+  const AlgorithmType getType() override;
+  const char* getTypeString() override;
 
-    void Init() override;
-    void Clear() override;
-    double Launch() override;
+  void Init() override;
+  void Clear() override;
+  double Launch() override;
 
-    void StepBegin(Branch*) override;
-    void StepEnd(Branch*, hpx_t) override;
-    void Run(Branch*, const void*) override;
-    hpx_t SendSpikes(Neuron* b, double tt, double t) override;
-    void AfterReceiveSpikes( Branch *local, hpx_t target, neuron_id_t preNeuronId,
-                             spike_time_t spikeTime, spike_time_t maxTime) override;
+  void StepBegin(Branch*) override;
+  void StepEnd(Branch*, hpx_t) override;
+  void Run(Branch*, const void*) override;
+  hpx_t SendSpikes(Neuron* b, double tt, double t) override;
+  void AfterReceiveSpikes(Branch* local, hpx_t target, neuron_id_t preNeuronId,
+                          spike_time_t spikeTime,
+                          spike_time_t maxTime) override;
 
+  /// controls time-dependencies from incoming neuron connections
+  class TimeDependencies : public AlgorithmMetaData {
+   public:
+    TimeDependencies();
+    ~TimeDependencies();
 
-    ///controls time-dependencies from incoming neuron connections
-    class TimeDependencies : public AlgorithmMetaData
-    {
-      public:
-        TimeDependencies();
-        ~TimeDependencies();
+    void WaitForTimeDependencyNeurons(floble_t t, floble_t dt, int gid);
 
-        void WaitForTimeDependencyNeurons(floble_t t, floble_t dt, int gid);
-        void SendSteppingNotification(floble_t t, floble_t dt, int gid, std::vector<Neuron::Synapse*> & synapses); ///> inform my outgoing-connection neurons that I stepped
-        void UpdateTimeDependency(neuron_id_t srcGid, floble_t dependencyNotificationTime, neuron_id_t myGid = -1, bool initialization = false);
-        floble_t GetDependenciesMinTime();
-        size_t GetDependenciesCount();
-        void IncreseDependenciesTime(floble_t t);
+    /// inform my outgoing-connection neurons that I stepped
+    void SendSteppingNotification(floble_t t, floble_t dt, int gid,
+                                  std::vector<Neuron::Synapse*>& synapses);
 
-        static floble_t notificationIntervalRatio; ///> ratio of notification interval (0,1]
-        static double teps; ///>time-epsilon to correct wrong delivery of events due to floating point rounding
+    /// update time of a given dependency
+    void UpdateTimeDependency(neuron_id_t srcGid,
+                              floble_t dependencyNotificationTime,
+                              neuron_id_t myGid = -1,
+                              bool initialization = false);
 
-      private:
-        std::map<neuron_id_t, floble_t> dependenciesMap; ///> map to previous structure (pointing to vector values)
-        libhpx_cond_t dependenciesWaitCondition;
-        libhpx_mutex_t dependenciesLock;
-        floble_t dependenciesTimeNeuronWaitsFor;
-    };
+    /// get smallest time across all dependencies
+    floble_t GetDependenciesMinTime();
+
+    /// get number of dependencies
+    size_t GetDependenciesCount();
+
+    /// increase time of all dependencies by 't'
+    void IncreseDependenciesTime(floble_t t);
+
+    /// ratio of notification interval (0,1]
+    static floble_t notificationIntervalRatio;
+
+    /// time-epsilon to correct wrong delivery of events due to floating point
+    /// rounding
+    static double teps;
+
+   private:
+    ///> map of actual time per dependency if
+    std::map<neuron_id_t, floble_t> dependenciesMap;
+    libhpx_cond_t dependenciesWaitCondition;
+    libhpx_mutex_t dependenciesLock;
+    floble_t dependenciesTimeNeuronWaitsFor;
+  };
 };
 
-}; //algorithm
+};  // algorithm
 
-}; //neurox
+};  // neurox
