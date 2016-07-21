be6977631ab467d6f8e67ec84eee829388413389
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2016-07-21 21:23:55 +0200

    changed notation of enums, according to google standards

--- neurox/algorithms/AllReduceAlgorithm.h
+++ neurox/algorithms/AllReduceAlgorithm.h
@@ -12,8 +12,8 @@ class AllReduceAlgorithm : public Algorithm {
   AllReduceAlgorithm();
   ~AllReduceAlgorithm();
 
-  const AlgorithmType getType() override;
-  const char* getTypeString() override;
+  const AlgorithmType GetType() override;
+  const char* GetTypeString() override;
 
   void Init() override;
   void Clear() override;
