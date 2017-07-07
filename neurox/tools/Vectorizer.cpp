9badda9e341ee0a587ac57a60aedf94cd0f80bad
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2017-07-07 14:59:38 +0200

    added pointers for thvar_ptr and VecPlayContinuousX in Vectorize::vectorize()

--- neurox/tools/Vectorizer.cpp
+++ neurox/tools/Vectorizer.cpp
@@ -22,13 +22,18 @@ void Tools::Vectorizer::vectorize(Branch * b)
    int N = b->nt->end;
    size_t totalDataSize = 6*sizeof_(N);
    assert(totalDataSize %NEUROX_MEM_ALIGNMENT==0);
-   int *  dataNewOffset = new int[b->nt->_ndata];
+   //int *  dataNewOffset = new int[b->nt->_ndata];
+   std::vector<int> dataNewOffset;
    for (int m=0; m<mechanismsCount; m++)
        totalDataSize += b->mechsInstances[m].nodecount * sizeof_(mechanisms[m]->dataSize);
 
-   double* data_new = new_<double>(totalDataSize);
+   //old thvar offset
+   int thvar_idx = b->thvar_ptr ? b->thvar_ptr - &b->nt->_actual_v[0] : -1; //compartment id (typically 2)
+   assert(thvar_idx==-1 || thvar_idx>=0 && thvar_idx < N);
 
+   double* data_new = new_<double>(totalDataSize);
    size_t dataOffset =0;
+
    //add padding to data for RHS, D, A, B, V and area
    for (int i=0; i<6; i++)
    {
@@ -46,11 +51,8 @@ void Tools::Vectorizer::vectorize(Branch * b)
             if (j < b->nt->end)
             {
                 data_new[dataOffset] = b->nt->_data[i*N+j];
-                dataNewOffset[i*N+j] = dataOffset;
-
-                //convert AP-threshold pointer (if held in this voltage)
-                if (i==4 && &(b->nt->_data[i*N+j]) == b->thvar_ptr)
-                    b->thvar_ptr = &data_new[dataOffset];
+                //dataNewOffset[i*N+j] = dataOffset;
+                dataNewOffset.push_back(dataOffset);
             }
    }
 
@@ -63,9 +65,27 @@ void Tools::Vectorizer::vectorize(Branch * b)
        for (int i=0; i<instances->nodecount; i++)
            for (size_t d=0; d<sizeof_(dataSize); d++, dataOffset++)
                if (d < dataSize)
+               {
                    data_new[dataOffset] = instances->data[dataSize*i+d];
+                   dataNewOffset.push_back(dataOffset);
+               }
        instances->data = instance_data_new;
    }
+   dataNewOffset.resize(dataNewOffset.size());
+
+   //convert AP-threshold pointer
+   b->thvar_ptr = b->thvar_ptr ? &b->nt->_actual_v[thvar_idx] : nullptr;
+
+   //convert VecPlay continuous pointers
+   for (int v=0; v<b->nt->n_vecplay; v++)
+   {
+       VecPlayContinuousX * vc = (VecPlayContinuousX*) b->nt->_vecplay[v];
+       int vc_offset = vc->pd_ - &b->nt->_data[0];
+       assert(vc_offset>=0 && vc_offset <= b->nt->_ndata);
+       int vc_offset_new = dataNewOffset.at(vc_offset);
+       vc->pd_ = &data_new[ vc_offset_new ];
+   }
+
    delete_(b->nt->_data);
    b->nt->_data  = data_new;
 
@@ -83,7 +103,7 @@ void Tools::Vectorizer::vectorize(Branch * b)
                if (isPointer)
                {
                    int & pd = instances->pdata[pdataSize*i+p];
-                   pd = dataNewOffset[pd]; //point to new offset (with gaps)
+                   pd = dataNewOffset.at(pd); //point to new offset (with gaps)
                }
            }
     }
