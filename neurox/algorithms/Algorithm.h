502eab10e1c367f639a45212ab4f2a2bc0998143
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2017-07-23 13:15:20 +0200

    added remaining headers of algorithms

--- neurox/algorithms/Algorithm.h
+++ neurox/algorithms/Algorithm.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "neurox.h"
 
-using namespace  neurox;
+using namespace neurox;
 
 namespace neurox
 {
@@ -10,15 +10,17 @@ namespace algorithms
 {
 
 enum AlgorithmType {
-    BackwardEulerDebugWithCommBarrier=0,
-    BackwardEulerWithAllReduceBarrier=1,
-    BackwardEulerWithSlidingTimeWindow=2,
-    BackwardEulerWithTimeDependencyLCO=3,
+    BackwardEulerDebugMode=0,
+    BackwardEulerAllReduce=1,
+    BackwardEulerSlidingTimeWindow=2,
+    BackwardEulerTimeDependencyLCO=3,
     ALL=9 //All, except debug
 };
 
 class Algorithm
 {
+    constexpr virtual AlgorithmType getType() = 0;
+
     virtual void init(Branch*) = 0;
     virtual void clear(Branch*) = 0;
     virtual void stepBegin(Branch*) = 0;
