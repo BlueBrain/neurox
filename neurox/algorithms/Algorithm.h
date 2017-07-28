cd48d310964c10b1afe3ea0d4bb5000f8a7ab494
Bruno Magalhaes <bruno.magalhaes@epfl.ch>
2017-07-28 14:40:18 +0200

    clang-format of ./algorithms/*

--- neurox/algorithms/Algorithm.h
+++ neurox/algorithms/Algorithm.h
@@ -2,68 +2,67 @@
 #include "neurox.h"
 #include "neurox/algorithms/AlgorithmMetaData.h"
 
-namespace neurox
-{
+namespace neurox {
 
-namespace algorithms
-{
+namespace algorithms {
 
 /**
  * @brief The Algorithm class
- * Virtual class describing all methods required to be implemented by all different algorithms
+ * Virtual class describing all methods required to be implemented by all
+ * different algorithms
  */
-class Algorithm
-{
-  public:
-    Algorithm() {};
-    virtual ~Algorithm() {};
+class Algorithm {
+ public:
+  Algorithm(){};
+  virtual ~Algorithm(){};
 
-    /// Returns total count of steps of fixed size defined by user
-    static int getTotalStepsCount();
+  /// Returns total count of steps of fixed size defined by user
+  static int getTotalStepsCount();
 
-    /// Returns an instantiated class of the given type
-    static Algorithm* New(AlgorithmType);
+  /// Returns an instantiated class of the given type
+  static Algorithm* New(AlgorithmType);
 
-    /// Prints info on algorithm, data size, and steps count
-    void PrintStartInfo();
+  /// Prints info on algorithm, data size, and steps count
+  void PrintStartInfo();
 
-    /// Returns class type
-    const virtual AlgorithmType getType() = 0;
+  /// Returns class type
+  const virtual AlgorithmType getType() = 0;
 
-    /// Returns class type as string
-    const virtual char* getTypeString() = 0;
+  /// Returns class type as string
+  const virtual char* getTypeString() = 0;
 
-    /// Launch simulation on all neurons or localities
-    virtual double Launch() = 0;
+  /// Launch simulation on all neurons or localities
+  virtual double Launch() = 0;
 
-    /// Runs simulation for given branch
-    virtual void Run(Branch*, const void*) = 0;
+  /// Runs simulation for given branch
+  virtual void Run(Branch*, const void*) = 0;
 
-    /// Initialize algorithm meta data
-    virtual void Init() {};
+  /// Initialize algorithm meta data
+  virtual void Init(){};
 
-    /// Clears/finalizes algorithm meta data
-    virtual void Clear() {};
+  /// Clears/finalizes algorithm meta data
+  virtual void Clear(){};
 
-    /// To be called at beginning of step
-    virtual void StepBegin(Branch*) {};
+  /// To be called at beginning of step
+  virtual void StepBegin(Branch*){};
 
-    /// To be called at end of step
-    virtual void StepEnd(Branch*, hpx_t spikesLco) {};
+  /// To be called at end of step
+  virtual void StepEnd(Branch*, hpx_t spikesLco){};
 
-    /// To handle sending of spikes
-    virtual hpx_t SendSpikes(Neuron*, double tt, double t) = 0;
+  /// To handle sending of spikes
+  virtual hpx_t SendSpikes(Neuron*, double tt, double t) = 0;
 
-    /// To handle receival of spikes
-    virtual void AfterReceiveSpikes(Branch *, hpx_t, neuron_id_t, spike_time_t, spike_time_t) {};
+  /// To handle receival of spikes
+  virtual void AfterReceiveSpikes(Branch*, hpx_t, neuron_id_t, spike_time_t,
+                                  spike_time_t){};
 };
 
-}; //algorithms
+};  // algorithms
 
-}; //neurox
+};  // neurox
 
-#include "neurox/algorithms/DebugAlgorithm.h"
-#include "neurox/algorithms/CoreneuronAlgorithm.h"
 #include "neurox/algorithms/AllReduceAlgorithm.h"
+#include "neurox/algorithms/CoreneuronAlgorithm.h"
+#include "neurox/algorithms/DebugAlgorithm.h"
 #include "neurox/algorithms/SlidingTimeWindowAlgorithm.h"
 #include "neurox/algorithms/TimeDependencyLCOAlgorithm.h"
--- neurox/algorithms/SlidingTimeWindowAlgorithm.h
+++ neurox/algorithms/SlidingTimeWindowAlgorithm.h
@@ -3,36 +3,33 @@
 
 using namespace neurox;
 
-namespace neurox
-{
+namespace neurox {
 
-namespace algorithms
-{
+namespace algorithms {
 
-class SlidingTimeWindowAlgorithm : public Algorithm
-{
-  public:
-    SlidingTimeWindowAlgorithm();
-    ~SlidingTimeWindowAlgorithm();
+class SlidingTimeWindowAlgorithm : public Algorithm {
+ public:
+  SlidingTimeWindowAlgorithm();
+  ~SlidingTimeWindowAlgorithm();
 
-    const AlgorithmType getType() override;
-    const char* getTypeString() override;
+  const AlgorithmType getType() override;
+  const char* getTypeString() override;
 
-    void Init() override;
-    void Clear() override;
-    double Launch() override;
+  void Init() override;
+  void Clear() override;
+  double Launch() override;
 
-    void StepBegin(Branch*) override;
-    void StepEnd(Branch*, hpx_t) override;
-    void Run(Branch*, const void*) override;
-    hpx_t SendSpikes(Neuron*, double, double) override;
+  void StepBegin(Branch*) override;
+  void StepEnd(Branch*, hpx_t) override;
+  void Run(Branch*, const void*) override;
+  hpx_t SendSpikes(Neuron*, double, double) override;
 
-    const size_t allReducesCount = 2;
-    static hpx_t * allReduces;
+  const size_t allReducesCount = 2;
+  static hpx_t* allReduces;
 
-  private:
+ private:
 };
 
-}; //algorithm
+};  // algorithm
 
-}; //neurox
+};  // neurox
